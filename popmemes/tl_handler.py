cd383a831bbd26f8f07c5639f14bee534353e1f3
Hakeem Angulu <hakeem.angulu@gmail.com>
2019-01-06 01:36:39 -0500

    include function to download all images on the timeline

--- popmemes/tl_handler.py
+++ popmemes/tl_handler.py
@@ -7,6 +7,7 @@ https://miguelmalvarez.com/2015/03/03/download-the-pictures-from-a-twitter-feed-
 
 import tweepy
 import json
+import wget
 
 # Import OAuthHandler parameters
 from auth import *
@@ -23,5 +24,23 @@ for status in tweepy.Cursor(api.home_timeline).items(25):
     media = status.entities.get('media', [])
     # Add non-empty media to the set
     if media:
-        print(media[0]['media_url'])
         media_files.add(media[0]['media_url'])
+
+def downloader(urls, path):
+    """
+    Given a set of urls, downloads the files to a given path.
+
+    urls (set): A set of urls.
+    path (string): A local path for storage.
+    """
+    counter = 1
+    for media_file in urls:
+        file_name = "test_img" + str(counter) + ".jpg"
+        file_location = path + "/" + file_name
+        print(f"Downloading {media_file} as {file_name}.")
+        wget.download(media_file, out=file_location)
+        print("\n")
+        counter += 1
+    print(f"{counter} items were downloaded.")
+
+downloader(media_files, '/Users/hakeemangulu/Code/twitter/popmemes/downloads')
