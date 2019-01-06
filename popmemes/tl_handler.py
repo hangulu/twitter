46a3c18dc80877e20c006efb480471490c8fd7cf
Hakeem Angulu <hakeem.angulu@gmail.com>
2019-01-06 01:12:13 -0500

    return urls of images on tl

--- popmemes/tl_handler.py
+++ popmemes/tl_handler.py
@@ -8,30 +8,20 @@ https://miguelmalvarez.com/2015/03/03/download-the-pictures-from-a-twitter-feed-
 import tweepy
 import json
 
-# Setup tweepy to return json
-
-# OAuthHandler parameters
-consumer_key = 'YOUR-CONSUMER-KEY'
-consumer_secret = 'YOUR-CONSUMER-SECRET'
-access_token = 'YOUR-ACCESS-TOKEN'
-access_secret = 'YOUR-ACCESS-SECRET'
-
-@classmethod
-def parse(cls, api, raw):
-    status = cls.first_parse(api, raw)
-    setattr(status, 'json', json.dumps(raw))
-    return status
-
-# Status() is the data model for a tweet
-tweepy.models.Status.first_parse = tweepy.models.Status.parse
-tweepy.models.Status.parse = parse
-
-# User() is the data model for a user profile
-tweepy.models.User.first_parse = tweepy.models.User.parse
-tweepy.models.User.parse = parse
+# Import OAuthHandler parameters
+from auth import *
 
 # Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
-
 api = tweepy.API(auth)
+
+# Store the media urls in a set
+media_files = set()
+# Iterate through the timeline and extract images
+for status in tweepy.Cursor(api.home_timeline).items(25):
+    media = status.entities.get('media', [])
+    # Add non-empty media to the set
+    if media:
+        print(media[0]['media_url'])
+        media_files.add(media[0]['media_url'])
