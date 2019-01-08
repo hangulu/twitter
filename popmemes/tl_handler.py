c3b9f908ab273d98dd651259119e1ca05874dfae
Hakeem Angulu <hakeem.angulu@gmail.com>
2019-01-08 11:13:48 -0500

    added the ability to specify a user

--- popmemes/tl_handler.py
+++ popmemes/tl_handler.py
@@ -17,22 +17,38 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
-def get_media(api, num_tweets=25):
+def get_media(api, num_tweets=25, profile="@hakeemangulu", admin=False):
     """
     Get all the media files from the timeline.
 
     api (Tweepy API object): The authentiated API access object.
     num_tweets (int): The number of tweets to scan on the timeline.
+    profile (string): The Twitter handle of the user to be analyzed.
+    admin (boolean): Whether the administrator is using the application.
     return: a set of urls for media
     """
     # Store the media urls in a list
     media_files = []
+
+    # Create cursor object for the timeline
+    if admin:
+        # If the admin is using the application, return his timeline
+        tl = tweepy.Cursor(api.home_timeline).items(num_tweets)
+    else:
+        # If the admin is not using the application, return the specified
+        # user's timeline
+        tl = tweepy.Cursor(api.user_timeline, screen_name=profile).items(num_tweets)
+
     # Iterate through the timeline and extract images
-    for status in tweepy.Cursor(api.home_timeline).items(num_tweets):
+    for status in tl:
+        # Get all media from a tweet
         media = status.entities.get('media', [])
         # Add non-empty media to the set
-        if media:
-            media_files.append(media[0]['media_url'])
+        for image in media:
+            # Only add the image if it is a photo or GIF (as opposed to a
+            # video)
+            if image['type'] == 'photo' or image['type'] == 'animated_gif':
+                media_files.append(image['media_url'])
     return media_files
 
 def downloader(urls, path):
@@ -59,7 +75,7 @@ def downloader(urls, path):
     print(f"{counter - 1} items were downloaded.")
     return counter - 1
 
-def get_and_download(api, path, num_tweets=25):
+def get_and_download(api, path, num_tweets=25, profile="@hakeemangulu", admin=False):
     """
     A combination of the get_media and downloader functions, thus finding
     the media then downloading it in one call.
@@ -67,6 +83,7 @@ def get_and_download(api, path, num_tweets=25):
     api (Tweepy API object): The authentiated API access object.
     num_tweets (int): The number of tweets to scan on the timeline.
     path (string): A local path for storage.
+    profile (string): The Twitter handle of the user to be analyzed.
     return: the number of files downloaded
     """
-    return downloader(get_media(api, num_tweets), path)
+    return downloader(get_media(api, num_tweets, profile, admin), path)
