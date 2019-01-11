09df56742ba0bdd0d8e9051cfb58baff21cad944
Hakeem Angulu <hakeem.angulu@gmail.com>
2019-01-11 18:26:28 -0500

    include views

--- popmemes/backend/popmemes/views.py
+++ popmemes/backend/popmemes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import MemeSerializer
+from .models import Popmemes
 
 # Create your views here.
+class MemeView(viewsets.ModelViewSet):
+  serializer_class = MemeSerializer
+  queryset = Popmemes.objects.all()
