0db9ac8aff6b73a2efa78fb87ac0d7e83d5f09b1
Hakeem Angulu <hakeem.angulu@gmail.com>
2019-01-17 04:25:44 -0500

    fix equality

--- popmemes/backend/popmemes/views.py
+++ popmemes/backend/popmemes/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view
 from .serializers import MemeSerializer
 from .models import Popmemes
 
-import find_memes as fm
+import popmemes.find_memes as fm
 
 # Create your views here.
 class MemeView(viewsets.ModelViewSet):
