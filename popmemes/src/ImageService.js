a7f1cbd6d18a0bb9057f8be15cd9d9dc7c20af39
Hakeem Angulu <hakeem.angulu@gmail.com>
2019-01-19 13:08:51 -0500

    add a js file for handling axios

---
+++ popmemes/src/ImageService.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+const API_URL = 'http://localhost:8000';
+
+export default class ImageService {
+  constructor(){}
+
+  // Get all popmemes
+  getPopmemes() {
+      const url = `${API_URL}/api/popmemes/`;
+      return axios.get(url).then(response => response.data);
+  }
+
+  // Get a single popmeme, by its username
+  getPopmeme(user) {
+      const url = `${API_URL}/api/popmemes/${user}`;
+      return axios.get(url).then(response => response.data);
+  }
+
+  // Delete a single popmeme, by its username
+  deletePopmeme(user) {
+      const url = `${API_URL}/api/popmemes/${user}`;
+      return axios.delete(url);
+  }
+
+  // Add a new popmeme
+  createPopmeme(user) {
+      const url = `${API_URL}/api/popmemes/`;
+      return axios.post(url, user);
+  }
+
+  // Update a meme
+  updatePopmeme(user){
+      const url = `${API_URL}/api/user/${user}`;
+      return axios.put(url, user);
+  }
+}
