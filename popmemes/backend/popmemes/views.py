601fb165cce52886cf325ce33bc5180b8b1b4895
Hakeem Angulu <hakeem.angulu@gmail.com>
2019-01-19 12:55:01 -0500

    use function based views

--- popmemes/backend/popmemes/views.py
+++ popmemes/backend/popmemes/views.py
@@ -17,6 +17,9 @@ import popmemes.find_memes as fm
 def show_popmeme(request):
     """
     Show the most popular meme on the timeline.
+
+    request (HTTP request): Data submitted via the form.
+    return: data with HTTP response
     """
     # When the user hits submit
     if request.method == 'POST':
@@ -26,6 +29,7 @@ def show_popmeme(request):
         # Serialize the response, check its validity, then save
         serializer = MemeSerializer({'user': username, 'pop_img': image, 'freq': freq})
         if serializer.is_valid():
+            # Save the serialized data in the database
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
@@ -39,3 +43,36 @@ def show_popmeme(request):
         # Serialize the meme then return it
         serializer = MemeSerializer(meme)
         return Response(serializer.data)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def popmeme_detail(request, user):
+    """
+    Retrieve, update or delete a user by their username.
+
+    request (HTTP request): Data submitted via the form.
+    user (string): The username to be examined.
+    return: data with HTTP response
+    """
+    try:
+        meme = Popmemes.objects.get(user=user)
+    except ValueError:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        # Get the information on this user
+        serializer = MemeSerializer(user, context={'request': request})
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        # Update this user
+        serializer = MemeSerializer(user, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            # Save to the database
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        # Delete this user
+        meme.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
