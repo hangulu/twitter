9f6133f503dda48a97d5e188a14dc18a8f263c41
Hakeem Angulu <hakeem.angulu@gmail.com>
2019-01-19 12:37:56 -0500

    move to function based views

--- popmemes/backend/popmemes/views.py
+++ popmemes/backend/popmemes/views.py
@@ -7,35 +7,35 @@ from .models import Popmemes
 
 import popmemes.find_memes as fm
 
-# Create your views here.
-class MemeView(viewsets.ModelViewSet):
-    serializer_class = MemeSerializer
-    lookup_field = 'user'
-    queryset = Popmemes.objects.all()
+# # Create your views here.
+# class MemeView(viewsets.ModelViewSet):
+#     serializer_class = MemeSerializer
+#     lookup_field = 'user'
+#     queryset = Popmemes.objects.all()
 
-    @api_view(['POST', 'GET'])
-    def show_popmeme(request):
-        """
-        Show the most popular meme on the timeline.
-        """
-        # When the user hits submit
-        if request.method == 'POST':
-            username = request.data
-            image, freq = fm.memr(username)
-            # image, freq = "meme1", 25.
-            # Serialize the response, check its validity, then save
-            serializer = MemeSerializer({'user': username, 'pop_img': image, 'freq': freq})
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        elif request.method == 'GET':
-            # Get the meme by the username
-            try:
-                meme = Popmemes.objects.get(user=request.data)
-            except ValueError:
-                return Response(status=status.HTTP_404_NOT_FOUND)
-            # Serialize the meme then return it
-            serializer = MemeSerializer(meme)
-            return Response(serializer.data)
+@api_view(['POST', 'GET'])
+def show_popmeme(request):
+    """
+    Show the most popular meme on the timeline.
+    """
+    # When the user hits submit
+    if request.method == 'POST':
+        username = request.data
+        # image, freq = fm.memr(username)
+        image, freq = "meme2", 5.
+        # Serialize the response, check its validity, then save
+        serializer = MemeSerializer({'user': username, 'pop_img': image, 'freq': freq})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        # Get the meme by the username
+        try:
+            meme = Popmemes.objects.get(user=request.data)
+        except ValueError:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        # Serialize the meme then return it
+        serializer = MemeSerializer(meme)
+        return Response(serializer.data)
