df3ed67d85d29fff9977f4895f6850fcf452f1f7
Hakeem Angulu <hakeem.angulu@gmail.com>
2019-01-20 19:22:29 -0500

    add better form props

--- popmemes/src/components/Form.jsx
+++ popmemes/src/components/Form.jsx
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
 import axios from "axios";
+import ImageService from './ImageService';
+
+// Instantiate the ImageService module
+const imservice = new ImageService();
 
 class Form extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: '', image: '', freq: '', run: 'false'};
+    this.state = {value: '', user: '', image: '', freq: ''};
 
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.displayData = this.displayData.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleKeyUp = this.handleKeyUp.bind(this);
+
     this.analyzing = this.analyzing.bind(this);
-    this.renderMeme = this.renderMeme.bind(this);
+  }
+
+  // Called when the component is created and inserted into the DOM
+  componentDidMount() {
+    var self = this;
+    imservice.getPopmemes()
+      .then(result => self.setState({image: 'No Image'}));
+  }
+
+  // Handle the submission of the form
+  handleSubmit(user) {
+    var self = this;
+    alert('Now analyzing the following Twitter user: @' + user);
+    // event.preventDefault();
+    // Post the meme with the username
+    imservice.createPopmeme(user)
+      .then(() => self.setState({user: user}))
+      .then(() => displayData(self.state.user));
+  }
+
+  // Display a user's data
+  displayData(user) {
+    var self = this;
+    imservice.getPopmeme(user)
+      // Set the properties using the response dictionary
+      .then(res => self.setState({image: res['pop_img'], freq: res['freq']}))
+      .then(() => alert("The most popular image on @" +  self.state.user + "'s timeline is " + self.state.image + " with a frequency of " + self.state.freq + "."))
+      .catch(err => console.log(err));
   }
 
   // Buffer between submission and analysis
@@ -19,7 +53,7 @@ class Form extends Component {
       // Get the meme with the specified username
       .get("http://localhost:8000/api/popmemes/", {params: {user: this.state.value}})
       // Handle the response, and set run to true
-      .then(res => this.setState({ image: res.data[0], freq: res.data[1], run: true }))
+      .then(res => this.setState({ image: res.data[0], freq: res.data[1]}))
       .then(res => alert("The most popular image on @" +  this.state.value + "'s timeline is " + this.state.image + " with a frequency of " + this.state.freq + "."))
       // .then(res => this.renderMeme())
       .catch(err => console.log(err));
@@ -31,39 +65,18 @@ class Form extends Component {
   }
 
   // Handle the submission of the form
-  handleSubmit(event) {
-    alert('Now analyzing the following Twitter user: @' + this.state.value);
-    event.preventDefault();
-    axios
-      // Post the meme with the username
-      .post("http://localhost:8000/api/popmemes/", {user: this.state.value, image: 'meme12', freq: 10.})
-      .then(res => this.analyzing());
-  }
+  // handleSubmit(event) {
+  //   alert('Now analyzing the following Twitter user: @' + this.state.value);
+  //   event.preventDefault();
+  //   axios
+  //     // Post the meme with the username
+  //     .post("http://localhost:8000/api/popmemes/", {user: this.state.value, image: 'meme12', freq: 10.})
+  //     .then(res => this.analyzing());
+  // }
 
   // Allow one to submit the form by hitting Enter
   handleKeyUp(event) {
     if (event.keyCode === 13) return this.sendData();
-  }
-
-  renderMeme() {
-    // Make variables to store the image and frequency
-    const image = this.state.image
-    const freq = this.state.freq
-    const user = this.state.value
-
-    if (this.state.run === true) {
-      return (
-        <span>
-        An analysis of the profile of {user} shows that {image} is the most popular image, with a frequency of {freq}.
-        </span>
-      );
-    } else {
-      return (
-        <span>
-        Waiting on input.
-        </span>
-      );
-    }
   }
 
   render() {
