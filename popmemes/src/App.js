874d89953d2784e2d0e53af32482a3c7b05d9ccc
Hakeem Angulu <hakeem.angulu@gmail.com>
2019-01-22 06:27:44 -0500

    complete frontend

--- popmemes/src/App.js
+++ popmemes/src/App.js
@@ -9,9 +9,6 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Enter the Twitter handle of the user whose timeline you'd like to analyze:
-          </p>
           <Form />
         </header>
       </div>
