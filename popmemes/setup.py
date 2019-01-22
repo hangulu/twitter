4bf0c5673a5e773bc5f4cf207e8f95c6620e057e
Hakeem Angulu <hakeem.angulu@gmail.com>
2019-01-22 07:01:09 -0500

    remove unnecessary files

--- popmemes/setup.py
+++
@@ -1,7 +0,0 @@
-"""
-This module allows pip to install the directory as a package.
-"""
-
-from setuptools import setup, find_packages
-
-setup(name='popmemes', version='1.0', packages=find_packages())
